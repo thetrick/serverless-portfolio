1146a28b39738d30f1510c6d6419ae5b2ee6c584
Todd G. Hetrick <toddghetrick@gmail.com>
2017-09-08 14:01:21 -0400

    added SNS functionality to function to support messaging

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,5 +1,3 @@
-# pylint: disable-msg=C0103
-
 import io
 import boto3
 import mimetypes
@@ -8,23 +6,32 @@ import zipfile
 from botocore.client import Config
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
     
-    srcBucket = os.environ['srcBucket']
-    destBucket = os.environ['destBucket']
-    assetsAsZip = os.environ['assetAsZip']
-    
-    destination = s3.Bucket(destBucket)
-    source = s3.Bucket(srcBucket)
-    
-    ms = io.BytesIO()
-    source.download_fileobj(assetsAsZip, ms)
-    
-    with zipfile.ZipFile(ms) as myzip:
-        for nm in myzip.namelist():
-            print (nm)
-            obj = myzip.open(nm)
-            destination.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            destination.Object(nm).Acl().put(ACL='public-read')
-    
-    print ("Deployment Done!")
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+        
+        srcBucket = os.environ['srcBucket']
+        destBucket = os.environ['destBucket']
+        assetsAsZip = os.environ['assetAsZip']
+        topicArn = os.environ['topicArn']
+        topic = sns.Topic(topicArn)
+        
+        destination = s3.Bucket(destBucket)
+        source = s3.Bucket(srcBucket)
+        
+        ms = io.BytesIO()
+        source.download_fileobj(assetsAsZip, ms)
+        
+        with zipfile.ZipFile(ms) as myzip:
+            for nm in myzip.namelist():
+                print (nm)
+                obj = myzip.open(nm)
+                destination.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                destination.Object(nm).Acl().put(ACL='public-read')
+        
+        print ("Deployment Done!")
+        topic.publish(Subject="Portfolio Deployed!", Message="Portfolio deployed successfully!")
+    except:
+        topic.publish(Subject="Portfolio Deployment Failed!", Message="The Portfolio was not deployed successfully!")
+        raise
