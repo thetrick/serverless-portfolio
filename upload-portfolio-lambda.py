71bc07d9f93606d1acbe68f00557a2ec3a9c7306
Todd G. Hetrick <toddghetrick@gmail.com>
2017-09-08 13:36:20 -0400

    converted to lambda function

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,25 +1,30 @@
 # pylint: disable-msg=C0103
 
 import io
-import zipfile
 import boto3
-from botocore.client import Config
 import mimetypes
+import os
+import zipfile
+from botocore.client import Config
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-
-PORTFOLIO_BUCKET = 'portfolio.toddghetrick.info'
-BUILD_BUCKET = 'buildit.toddghetrick.info'
-
-myportfolio = s3.Bucket(PORTFOLIO_BUCKET)
-mybuild = s3.Bucket(BUILD_BUCKET)
-
-ms = io.BytesIO()
-mybuild.download_fileobj('portfoliobuild/artifacts.zip', ms)
-
-with zipfile.ZipFile(ms) as myzip:
-    for nm in myzip.namelist():
-        print (nm)
-        obj = myzip.open(nm)
-        myportfolio.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        myportfolio.Object(nm).Acl().put(ACL='public-read')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    
+    srcBucket = os.environ['srcBucket']
+    destBucket = os.environ['destBucket']
+    assetsAsZip = os.environ['assetAsZip']
+    
+    destination = s3.Bucket(destBucket)
+    source = s3.Bucket(srcBucket)
+    
+    ms = io.BytesIO()
+    source.download_fileobj(assetsAsZip, ms)
+    
+    with zipfile.ZipFile(ms) as myzip:
+        for nm in myzip.namelist():
+            print (nm)
+            obj = myzip.open(nm)
+            destination.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            destination.Object(nm).Acl().put(ACL='public-read')
+    
+    print ("Deployment Done!")
